64b3c281bc93a2df4a6143bb006f79d1d10462e7
Jeric June Logan <jericjune.logan.lhoopa@gmail.com>
2023-05-16 23:30:27 +0800

    breeze inertia typescript setup

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,37 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: "resources/js/app.ts",
+            ssr: "resources/js/ssr.ts",
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    css: {
+        devSourcemap: true,
+    },
+    server: {
+        https: false,
+        host: true,
+        strictPort: true,
+        port: 5176,
+        hmr: {
+            host: "localhost",
+            protocol: "ws",
+        },
+        watch: {
+            usePolling: true,
+        },
+    },
 });
