8e761e0aa941df8048b3419b42fbdb8e6f2a4c3d
Jeric June Logan <jericjune.logan.lhoopa@gmail.com>
2023-06-12 21:44:12 +0800

    add husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-    root: true,
-    parserOptions: {
-        ecmaVersion: 2020, // Use the latest ecmascript standard
-        sourceType: 'module', // Allows using import/export statements
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020, // Use the latest ecmascript standard
+    sourceType: 'module', // Allows using import/export statements
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
-    env: {
-        browser: true,
-        amd: true,
-        node: true,
-    },
-    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-    plugins: ['prettier'],
-    rules: {
-        'prettier/prettier': [
-            'error',
-            {},
-            {
-                usePrettierrc: true,
-            },
-        ],
-        'no-console': 2,
-    },
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {},
+      {
+        usePrettierrc: true,
+      },
+    ],
+    'no-console': 2,
+  },
 };
