932a3a47935a126f1f4b0b5160a8fa9c827288c6
Jeric <jericjune.logan.lhoopa@gmail.com>
2023-06-29 14:54:41 +0800

    modified deploy script

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,10 +3,6 @@ set -e
 
 echo "Deployment started ..."
 
-# Enter maintenance mode or return true
-# if already is in maintenance mode
-(php artisan down) || true
-
 # Pull the latest version of the app
 git pull origin master
 
@@ -22,15 +18,11 @@ php artisan optimize
 # Resolve NVM
 export NVM_DIR=~/.nvm
 source ~/.nvm/nvm.sh
-yarn --help
 
 # Compile npm assets
 yarn build
 
 # Run database migrations
 php artisan migrate --force
-
-# Exit maintenance mode
-php artisan up
 
 echo "Deployment finished!"
